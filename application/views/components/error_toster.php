07f4516e6919af2e5293bc1b8536e901a9f21916
cronos <bamlakdesalgn@gmail.com>
2022-12-14 12:44:49 +0300

    validation for client done

--- application/views/components/error_toster.php
+++ application/views/components/error_toster.php
@@ -1,4 +1,4 @@
-<div class="alert alert-danger alert-dismissible fade show" role="alert">
+<div class="alert alert-danger alert-dismissible p-0 m-0" role="alert">
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     <?= $error?>
 </div>
--- application/views/shared/footer.php
+++ application/views/shared/footer.php
@@ -36,24 +36,27 @@
 <script src="<?php echo base_url(); ?>assets/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.min.js"></script>
 <script src="<?php echo base_url(); ?>assets/libs/admin-resources/jquery.vectormap/maps/jquery-jvectormap-world-mill-en.js"></script>
 <!-- third party js -->
-<script src="<?= base_url();?>assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="<?= base_url();?>assets/libs/datatables.net-bs5/js/dataTables.bootstrap5.min.js"></script>
-<script src="<?= base_url();?>assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-<script src="<?= base_url();?>assets/libs/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js"></script>
-<script src="<?= base_url();?>assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-<script src="<?= base_url();?>assets/libs/datatables.net-buttons-bs5/js/buttons.bootstrap5.min.js"></script>
-<script src="<?= base_url();?>assets/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
-<script src="<?= base_url();?>assets/libs/datatables.net-buttons/js/buttons.flash.min.js"></script>
-<script src="<?= base_url();?>assets/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
-<script src="<?= base_url();?>assets/libs/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
-<script src="<?= base_url();?>assets/libs/datatables.net-select/js/dataTables.select.min.js"></script>
-<!-- <script src="<?= base_url();?>assets/libs/pdfmake/build/pdfmake.min.js"></script> -->
-<!-- <script src="<?= base_url();?>assets/libs/pdfmake/build/vfs_fonts.js"></script> -->
+<script src="<?= base_url(); ?>assets/libs/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="<?= base_url(); ?>assets/libs/datatables.net-bs5/js/dataTables.bootstrap5.min.js"></script>
+<script src="<?= base_url(); ?>assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+<script src="<?= base_url(); ?>assets/libs/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js"></script>
+<script src="<?= base_url(); ?>assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+<script src="<?= base_url(); ?>assets/libs/datatables.net-buttons-bs5/js/buttons.bootstrap5.min.js"></script>
+<script src="<?= base_url(); ?>assets/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
+<script src="<?= base_url(); ?>assets/libs/datatables.net-buttons/js/buttons.flash.min.js"></script>
+<script src="<?= base_url(); ?>assets/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
+<script src="<?= base_url(); ?>assets/libs/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
+<script src="<?= base_url(); ?>assets/libs/datatables.net-select/js/dataTables.select.min.js"></script>
+<!-- <script src="<?= base_url(); ?>assets/libs/pdfmake/build/pdfmake.min.js"></script> -->
+<!-- <script src="<?= base_url(); ?>assets/libs/pdfmake/build/vfs_fonts.js"></script> -->
 <!-- third party js ends -->
-<script src="<?= base_url();?>assets/libs/ladda/spin.min.js"></script>
-<script src="<?= base_url();?>assets/libs/ladda/ladda.min.js"></script>
+<script src="<?= base_url(); ?>assets/libs/ladda/spin.min.js"></script>
+<script src="<?= base_url(); ?>assets/libs/ladda/ladda.min.js"></script>
 <!-- Datatables init -->
-<script src="<?= base_url();?>assets/js/pages/datatables.init.js"></script>
+<script src="<?= base_url(); ?>assets/js/pages/datatables.init.js"></script>
+
+<!-- Validation init js-->
+<script src="assets/js/pages/form-validation.init.js"></script>
 
 <!-- Dashboard 2 init -->
 <!-- <script src="<?php echo base_url(); ?>assets/libs/parsleyjs/parsley.min.js"></script> -->
